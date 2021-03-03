a7307ef0a1a6cbbbc889e9dfcdb065db866fabc7
Héctor Alarcón <hector.alarcon@wizeline.com>
2021-03-03 14:08:33 -0600

    local import issue

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -3,9 +3,9 @@ package router
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/halarcon-wizeline/academy-go-q12021/interface/controller"
+	// "github.com/halarcon-wizeline/academy-go-q12021/interface/controller"
 )
-
+/*
 func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -13,4 +13,4 @@ func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
 	e.GET("/pokemons", func(context echo.Context) error { return c.GetPokemons(context) })
 
 	return e
-}
+}*/
