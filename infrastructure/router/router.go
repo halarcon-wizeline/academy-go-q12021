d671fd19edc734d856b4fceb7cc80f98514d5988
Héctor Alarcón <hector.alarcon@wizeline.com>
2021-03-04 17:40:47 -0600

    Pokemon added

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -10,7 +10,7 @@ func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/pokemons", func(context echo.Context) error { return c.PokemonController.GetPokemons(context) })
+	e.GET("/pokemons", func(context echo.Context) error { return c.Pokemon.GetPokemons(context) })
 
 	return e
 }
