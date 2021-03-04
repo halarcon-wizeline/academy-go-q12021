aebbab2b9ab787c5b8fedc97ebf465e0905643c5
Héctor Alarcón <hector.alarcon@wizeline.com>
2021-03-04 13:32:49 -0600

    pokemons csv added

--- main.go
+++ main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	// "fmt"
+	// "log"
 	// "errors"
 
-	"github.com/labstack/echo"
+	// "github.com/labstack/echo"
 
 	// "github.com/halarcon-wizeline/academy-go-q12021/infrastructure/datastore"
 	// "github.com/halarcon-wizeline/academy-go-q12021/interface/controller"
-	"github.com/halarcon-wizeline/academy-go-q12021/registry"
+	// "github.com/halarcon-wizeline/academy-go-q12021/registry"
 
-	// "fmt"
-	// "log"
-	// "net/http"
-	// "encoding/json"
-	// "encoding/csv"
-	// "io"
-	// "os"
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"encoding/csv"
+	"io"
+	"os"
+
+	"github.com/halarcon-wizeline/academy-go-q12021/domain/model"
 
 )
 
+/*
 func main() {
 
-	r := registry.NewRegistry()
+	// r := registry.NewRegistry()
 
-/*
 	e := echo.New()
 	e = router.NewRouter(e, r.NewAppController())
 
@@ -34,36 +36,25 @@ func main() {
 	if err := e.Start(":" + serverPort); err != nil {
 		log.Fatalln(err)
 	}
+}
 	*/
-}
 
+type Pokemons []model.Pokemon
 
-/*
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
+func allPokemons(w http.ResponseWriter, r *http.Request) {
 
-type Articles []Article
+	pokemons := readCsvPokemons("./infrastructure/datastore/pokemons.csv")
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	// articles := Articles {
-	// 	Article {Title:"Test title", Desc:"Test description", Content:"Hello World"},
-	// }
-
-	articles := readCsvArticles("./infrastructure/datastore/articles.csv")
-
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+	fmt.Println("Endpoint Hit: All Pokemons Endpoint")
+	json.NewEncoder(w).Encode(pokemons)
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
-func readCsvArticles(file string) []Article {
-		// Open the file
+func readCsvPokemons(file string) []model.Pokemon {
+	// Open the file
 	csvfile, err := os.Open(file)
 	if err != nil {
 		log.Fatalln("Couldn't open the csv file", err)
@@ -72,7 +63,7 @@ func readCsvArticles(file string) []Article {
 	// Parse the file
 	r := csv.NewReader(csvfile)
 
-	var articles []Article
+	var pokemons []model.Pokemon
 	// Iterate through the records
 	for {
 		// Read each record from csv
@@ -83,19 +74,22 @@ func readCsvArticles(file string) []Article {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("Article: %s %s %s\n", record[0], record[1], record[2])
-		article := Article {Title:record[0], Desc:record[1], Content:record[2]}
-		articles = append(articles, article)
+		fmt.Printf("Pokemon: %s %s\n", record[0], record[1])
+		pokemon := model.Pokemon {ID:record[0], Name:record[1]}
+		pokemons = append(pokemons, pokemon)
 	}
-	return articles
+	return pokemons
 }
 
 func handleRequests() {
+	serverPort := "8081"
+	fmt.Println("Server listen at http://localhost" + ":" + serverPort)
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/pokemons", allPokemons)
+	log.Fatal(http.ListenAndServe(":"+serverPort, nil))
 }
 
 func main() {
 	handleRequests()
-}*/
+}
+
