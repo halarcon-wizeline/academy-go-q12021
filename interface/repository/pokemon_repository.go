1d86b641cfff278a287f3c1dd2cc71c2b7c53db3
Héctor Alarcón <hector.alarcon@wizeline.com>
2021-03-04 22:59:13 -0600

    GET endpoint added

--- interface/repository/pokemon_repository.go
+++ interface/repository/pokemon_repository.go
@@ -1,6 +1,11 @@
 package repository
 
 import (
+	"fmt"
+	"log"
+	"io"
+	"os"
+	"strconv"
 	"errors"
 	"github.com/halarcon-wizeline/academy-go-q12021/domain/model"
 )
@@ -17,12 +22,47 @@ func NewPokemonRepository() PokemonRepository {
 }
 
 func (ur *pokemonRepository) FindAll(u []*model.Pokemon) ([]*model.Pokemon, error) {
-	var ErrNotFound = errors.New("not found")
-	ErrNotFound = nil
 
-	if ErrNotFound != nil {
-		return nil, ErrNotFound
+	pokemons = readCsvPokemons("./infrastructure/datastore/pokemons.csv")
+
+	// Open the file
+	csvfile, err := os.Open(file)
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+		return nil, err
 	}
 
-	return u, nil
+	// Parse the file
+	r := csv.NewReader(csvfile)
+
+	var pokemons []model.Pokemon
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Reading pokemon: %s %s\n", record[0], record[1])
+		id, err := strconv.Atoi(record[0])
+		if err != nil {
+			log.Fatalln("Error: Pokemon: %s does not have a valid ID\n", record[1])
+		}
+		pokemon := model.Pokemon {ID:id, Name:record[1]}
+		pokemons = append(pokemons, pokemon)
+	}
+	return pokemons, nil
+
+	// var ErrNotFound = errors.New("not found")
+	// ErrNotFound = nil
+
+	// if ErrNotFound != nil {
+	// 	return nil, ErrNotFound
+	// }
+
+	// return u, nil
+
 }
