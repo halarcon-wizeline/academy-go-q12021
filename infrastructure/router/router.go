dc7251badbf539fbec432b8430a8c23efc8e7bcf
Héctor Alarcón <hector.alarcon@wizeline.com>
2021-03-09 11:17:03 -0600

    Param route debug

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
 	"github.com/halarcon-wizeline/academy-go-q12021/interface/controller"
 	"github.com/labstack/echo"
@@ -8,21 +9,25 @@ import (
 )
 
 func NewRouter(e *echo.Echo, c controller.AppController) *echo.Echo {
+	fmt.Printf("NewRouter")
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.GET("/pokemons",
 		func(context echo.Context) error {
+			fmt.Printf("pokemons")
 			return c.GetPokemons(context)
 	})
 	e.GET("/pokemons/:id",
 		func(context echo.Context) error {
+			fmt.Printf("pokemons/:id")
 			id := context.Param("id")
 			return context.String(http.StatusOK, id)
 	})
 
 	e.GET("/",
 		func(context echo.Context) error {
+			fmt.Printf("pokemons")
 			return context.String(http.StatusOK, "Hello, World!")
 	})
 
